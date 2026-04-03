1a141cfb27a8f6761bbfe651ab22d43e704b1547
Wally Pons <WallyPons@users.noreply.github.com>
2026-04-03 08:58:13 -0400

    Refactor SQL script for clarity and hardcode DB name

--- KillProcess.sql
+++ KillProcess.sql
@@ -5,13 +5,10 @@ your specific database name, copy the results and
 execute on a different window.
 */
 
--- 1. Declare variable with database name
-DECLARE @DBName VARCHAR(255) = 'msdb'
-
--- 2. Drop table if exists
+-- 1. Drop table if exists
 DROP TABLE IF EXISTS #SP_WHO2;
 
--- 3. Create temp table
+-- 2. Create temp table
 CREATE TABLE #SP_WHO2
 (SPID INT, Status VARCHAR(255),
  [Login] VARCHAR(255), HostName VARCHAR(255),
@@ -21,28 +18,28 @@ CREATE TABLE #SP_WHO2
  ProgramName VARCHAR(255), SPID1 INT,
  REQUESTID INT);
 
--- 4. Insert values into #SP_WHO2
+-- 3. Insert values into #SP_WHO2
 INSERT INTO #SP_WHO2 EXEC SP_WHO2;
 
--- 5. View contents of #SP_WHO2
+-- 4. View contents of #SP_WHO2
 SELECT * FROM #SP_WHO2;
 
--- 6. Add a filter to #SP_WHO2 table
+-- 5. Add a filter to #SP_WHO2 table
 SELECT *
 FROM #SP_WHO2
-WHERE DBName = @DBName
+WHERE DBName = 'msdb'
 
--- 7. Create KILL process for specific connections
+-- 6. Create KILL process for specific connections
 SELECT 
     'KILL ' + CAST(SPID 
     AS VARCHAR(4)) 
     AS [KILL Command]
 FROM #SP_WHO2
-WHERE DBName = @DBName
+WHERE DBName = 'msdb'
 /*
  Don't forget to download a copy of this and other 
  scripts from my Github repo, please check the link and 
- name of the file on the video description. 
- This and other solutions are given "AS IS" under no
- warranty or claim.
+ name of the file on the video description. This and 
+ other solutions are given "AS IS" under no  warranty 
+ or claim.
  */
