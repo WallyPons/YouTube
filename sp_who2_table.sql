b9a4b12097619758334a072110bfa7b5bdfb7016
Wally Pons <WallyPons@users.noreply.github.com>
2026-04-03 08:52:57 -0400

    Change DBName variable to hardcoded 'msdb'
    Changed the database name variable to a hardcoded value in the query filter.

--- sp_who2_table.sql
+++ sp_who2_table.sql
@@ -4,9 +4,6 @@ evaluation purposes. You can filter the data
 and create a summary of the connections too.
 */
 
--- 1. Declare variable with database name
-DECLARE @DBName VARCHAR(255) = 'msdb'
-
 -- 2. Drop table if exists
 DROP TABLE IF EXISTS #SP_WHO2;
 
@@ -29,12 +26,12 @@ SELECT * FROM #SP_WHO2;
 -- 6. Add a filter to #SP_WHO2 table
 SELECT *
 FROM #SP_WHO2
-WHERE DBName = @DBName
+WHERE DBName = 'msdb'
 
 /*
  Don't forget to download a copy of this and other 
  scripts from my Github repo, please check the link and 
- name of the file on the video description. 
- This and other solutions are given "AS IS" under no
- warranty or claim.
+ name of the file on the video description. This and 
+ other solutions are given "AS IS" under no  warranty 
+ or claim.
  */
