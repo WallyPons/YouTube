b261c7af3c9a7c6da1dd546736d175bfbe4cf027
Wally Pons <WallyPons@users.noreply.github.com>
2026-05-16 11:14:35 -0400

    Refactor SQL script to use #DIRFiles temp table

--- InsertFilesIntoTempTable.sql
+++ InsertFilesIntoTempTable.sql
@@ -9,11 +9,11 @@ RECONFIGURE;
 GO
 
 -- a. Drop/Create/insert to temp table
-DROP TABLE IF EXISTS #CMDFiles;
-CREATE TABLE #CMDFiles (Files varchar(max));
-INSERT INTO #CMDFiles
+DROP TABLE IF EXISTS #DIRFiles;
+CREATE TABLE #DIRFiles (Files varchar(max));
+INSERT INTO #DIRFiles
 
--- b. Execute CMD against a directory (C:\WINDOWS)
+-- b. Execute DIR against a directory (C:\WINDOWS)
 -- You can provide a different directory or path
 EXECUTE xp_cmdshell 'DIR C:\WINDOWS\ /b /a-d /on';
 GO
@@ -26,7 +26,7 @@ RECONFIGURE;
 GO
 
 -- 2. View files
-SELECT Files FROM #CMDFiles WHERE Files != 'NULL';
+SELECT Files FROM #DIRFiles WHERE Files != 'NULL';
 
 /*
 Don't forget to download a copy of this and other 
